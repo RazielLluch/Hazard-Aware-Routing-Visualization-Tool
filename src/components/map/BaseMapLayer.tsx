c70ec21ad994e085f93c4890e00c420ba5617bd1
Josiah Raziel S. Lluch <josiahraziel.lluch@g.msuiit.edu.ph>
2026-03-09 01:23:16 +0800

    changed tile set to CartoDB.Positron

--- src/components/map/BaseMapLayer.tsx
+++ src/components/map/BaseMapLayer.tsx
@@ -3,8 +3,8 @@ import { TileLayer } from "react-leaflet";
 export default function BaseMapLayer() {
     return (
         <TileLayer
-            url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
     );
 }
